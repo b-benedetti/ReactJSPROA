2633f8ed4fa9fe5fee66bac3976962b82ac1ee9c
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-30 11:25:42 -0300

    Add files via upload

---
+++ App01/App01.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+var a= parseInt(prompt("Digite um valor"));
+var b= parseInt(prompt("Digite um valor"));
+
+function soma() {
+return(a+b);
+}
+
+function App01 () {
+    return (
+        <div className="App01">
+            <p>A soma dos dois valores é de: {soma()}</p>
+        </div>
+    )
+}
+
+export default App01;
